315df86e871e84474ea0e74f7ef1895e1d6aa40d
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-08-20 16:12:05 -0500

    standard commits

--- projects/local/local.js
+++ projects/local/local.js
@@ -3,3 +3,24 @@ function fadeInPage() {
     var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
 }
+
+const element = document.querySelector('#myHeader')
+element.classList.add('animated', 'bounceOutLeft')
+
+element.addEventListener('animationend', function () {
+    // do something
+})
+
+function animateCSS(element, fadeIn, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('.animated', 'fadeIn')
+    
+    function handleAnimationEnd() {
+        node.classList.remove('animated', fadeIn)
+        node.removeEventListener('animationend', handleAnimationEnd)
+        
+        if (typeof callback === 'function') callback()
+    }
+    
+    node.addEventListener('animationend', handleAnimationEnd)
+}

a4d78822fb67f58de6578195660f11a4f1868794
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-09-03 15:24:10 -0500

    standard commits

--- projects/geo/relations.js
+++ projects/geo/relations.js
@@ -1,5 +1,25 @@
+/* exported fadeInPage */
+/* exported togAdmin */
+/* global document */
+/* global $ */
+/* global window */
+
 function fadeInPage() {
     if (!window.AnimationEvent) { return; }
     var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
+}
+
+function togAdmin(name) {
+    var strname = document.getElementById(name);
+    var iconname = document.getElementById(name+'Icon');
+    if (strname.style.display === 'none') {
+        $(strname).slideToggle(600)
+        strname.style.display = 'block';
+        $(iconname).removeClass('fa-plus').addClass('fa-minus');
+    }
+    else {
+        strname.style.display = 'none';
+        $(iconname).removeClass('fa-minus').addClass('fa-plus');
+    }
 }
